4ac25ab9e2241d0fb00f8ac406bcd36fb91e9565
ProudTurtle <a.wierzbicka0579@gmail.com>
2022-08-17 13:32:29 +0200

    Obsługa przycisku "+"

--- src/main/java/github/ProudTurtle/controller/ProjectController.java
+++ src/main/java/github/ProudTurtle/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package github.ProudTurtle.controller;
 
+import github.ProudTurtle.model.ProjectStep;
 import github.ProudTurtle.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +15,12 @@ public class ProjectController {
     @GetMapping
     String showProjects(Model model){
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current){
+        current.getSteps().add(new ProjectStep());
         return "projects";
     }
 }
