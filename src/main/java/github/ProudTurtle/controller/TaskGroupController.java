cbaaa5e689a676e26de9dad269beef708b4e3706
ProudTurtle <a.wierzbicka0579@gmail.com>
2022-08-25 23:19:15 +0200

    dodanie frontu

--- src/main/java/github/ProudTurtle/controller/TaskGroupController.java
+++ src/main/java/github/ProudTurtle/controller/TaskGroupController.java
@@ -1,21 +1,28 @@
 package github.ProudTurtle.controller;
 
 import github.ProudTurtle.logic.TaskGroupService;
+import github.ProudTurtle.model.ProjectStep;
 import github.ProudTurtle.model.Task;
 import github.ProudTurtle.model.TaskRepository;
 import github.ProudTurtle.model.projection.GroupReadModel;
+import github.ProudTurtle.model.projection.GroupTaskWriteModel;
 import github.ProudTurtle.model.projection.GroupWriteModel;
+import github.ProudTurtle.model.projection.ProjectWriteModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/groups")
 class TaskGroupController {
     private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
@@ -27,22 +34,55 @@ class TaskGroupController {
         this.repository = repository;
     }
 
-    @PostMapping
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    String showGroups(Model model) {
+        model.addAttribute("group", new GroupWriteModel());
+        return "groups";
+    }
+
+    @PostMapping(produces = MediaType.TEXT_HTML_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    String addGroup(
+            @ModelAttribute("group") @Valid GroupWriteModel current,
+            BindingResult bindingResult,
+            Model model
+
+    ) throws NoSuchFieldException, IllegalAccessException {
+        if (bindingResult.hasErrors()) {
+            return "groups";
+        }
+        service.createGroup(current);
+        model.addAttribute("group", new GroupWriteModel());
+        model.addAttribute("groups", getGroups());
+        model.addAttribute("message", "Dodano grupę!");
+        return "groups";
+    }
+
+
+    @PostMapping(params = "addTask", produces = MediaType.TEXT_HTML_VALUE)
+    String addGroupTask(@ModelAttribute("group") GroupWriteModel current) {
+        current.getTasks().add(new GroupTaskWriteModel());
+        return "groups";
+    }
+    @ResponseBody
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel toCreate) throws NoSuchFieldException, IllegalAccessException {
         GroupReadModel result = service.createGroup(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
-    @GetMapping
+    @ResponseBody
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<GroupReadModel>> readAllGroups() {
         return ResponseEntity.ok(service.readAll());
     }
 
-    @GetMapping("/{id}")
+    @ResponseBody
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<Task>> readAllTasksFromGroup(@PathVariable int id) {
         return ResponseEntity.ok(repository.findAllByGroup_Id(id));
     }
 
+    @ResponseBody
     @Transactional
     @PatchMapping("/{id}")
     public ResponseEntity<?> toggleGroup(@PathVariable int id) throws NoSuchFieldException, IllegalAccessException {
@@ -51,12 +91,17 @@ class TaskGroupController {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+    ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e) {
         return ResponseEntity.notFound().build();
     }
+
     @ExceptionHandler(IllegalStateException.class)
     ResponseEntity<String> handleIllegalState(IllegalStateException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
+    @ModelAttribute("groups")
+    List<GroupReadModel> getGroups() {
+        return service.readAll();
+    }
 }
--- src/main/java/github/ProudTurtle/model/projection/GroupTaskWriteModel.java
+++ src/main/java/github/ProudTurtle/model/projection/GroupTaskWriteModel.java
@@ -2,18 +2,23 @@ package github.ProudTurtle.model.projection;
 
 import github.ProudTurtle.model.Task;
 import github.ProudTurtle.model.TaskGroup;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
+
 public class GroupTaskWriteModel {
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime deadline;
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(final String description) {
         this.description = description;
     }
 
@@ -21,11 +26,11 @@ public class GroupTaskWriteModel {
         return deadline;
     }
 
-    public void setDeadline(LocalDateTime deadline) {
+    public void setDeadline(final LocalDateTime deadline) {
         this.deadline = deadline;
     }
 
-    public Task toTask(TaskGroup group){
+    Task toTask(final TaskGroup group) {
         return new Task(description, deadline, group);
     }
 }
